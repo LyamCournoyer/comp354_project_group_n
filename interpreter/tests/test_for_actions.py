6f1723399c9342f467a49324827c0c0466acfc11
-alex_kofman- <akofman123@gmail.com>
2021-12-03 22:53:01 -0500

    Start testing for command & add path appender imports for test files

--- interpreter/tests/test_for_actions.py
+++ interpreter/tests/test_for_actions.py
@@ -1,7 +1,8 @@
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import unittest
-import actions.if_action as if_action
+import actions.for_action as for_action
+import actions.set_action as set_action
 import variables
 from custom_exceptions import IncorrectSyntaxException
 
@@ -13,33 +14,44 @@ class ExpectedResult():
         self.expected_result = expected_result
         self.reason = reason
 
-class TestIfActions(unittest.TestCase):
+class TestForActions(unittest.TestCase):
 
+
+##TODO finish testing with setting before valid for loops are tested
     def test_parsing_and_actions(self):
-        lines_to_test = [
-            ExpectedResult('if 1 is less than 2 add 1 to 34', if_action.IfAction, 35, 'Less than condition is true'),
-            ExpectedResult('if 3 is greater than 1 add 27 to 27', if_action.IfAction, 54, 'Greater than condition is true'),
+        setup = [
+            'set x to 1',
+            'set y to 54',
         ]
-        
+
+        lines_to_test = [
+            ExpectedResult('for 2 times set x to add 10 to x',for_action.ForAction,20,'For loop twice')
+        ]
+
+        for line in setup:
+            parsed_action = set_action.SetAction.parse_from_line(line.split())
+            my_variables = variables.Variables()
+            action_res = parsed_action.action(my_variables)
+
         for expected_res in lines_to_test:
             my_variables = variables.Variables()
             with self.subTest(msg=f'Test {expected_res.reason}: "{expected_res.line}" is of type {expected_res.type} with result {expected_res.expected_result}'):
-                parsed_action = if_action.IfAction.parse_from_line(expected_res.items)
+                parsed_action = for_action.ForAction.parse_from_line(expected_res.items)
                 self.assertIsInstance(parsed_action, expected_res.type )
                 action_res = parsed_action.action(my_variables)
                 self.assertEqual(action_res, expected_res.expected_result)
 
     def test_exceptions(self):
         lines_expected_exception = [
-            'if 2 is greater than 2 add 1 to 34',
-            'if 3 is less than 2 add 1 to 34',
-            'if 3 is leeeeees than 2 add 1 to 34',
-            'ifeee 3 is leeeeees than 2 add 1 to 34',
+            'for 10 times set test_for to add zzzs to test_for',
+            'for zzz times set test_for to add zzzs to test_for',
+            'for 12 times add zz to zz',
         ]
 
         for expected_exception in lines_expected_exception:
             with self.assertRaises(IncorrectSyntaxException):
-                if_action.IfAction.parse_from_line(expected_exception)
+                for_action.ForAction.parse_from_line(expected_exception)
 
 if __name__ == '__main__':
     unittest.main()
+
