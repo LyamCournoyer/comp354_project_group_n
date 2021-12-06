ee00e20c36799350bade6c3303a4510c96a2e956
-alex_kofman- <akofman123@gmail.com>
2021-12-06 17:30:16 -0500

    Add working test_for_actions

--- interpreter/tests/test_for_actions.py
+++ interpreter/tests/test_for_actions.py
@@ -2,44 +2,36 @@ import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import unittest
 import actions.for_action as for_action
+import actions.math_actions as math_action
 import actions.set_action as set_action
 import variables
 from custom_exceptions import IncorrectSyntaxException
 
 class ExpectedResult():
-    def __init__(self, line, _type, expected_result, reason=''):
+    def __init__(self, line, _type, next_action,reason=''):
         self.line = line
         self.items = self.line.split()
         self.type = _type
-        self.expected_result = expected_result
+        self.next_action = next_action
         self.reason = reason
 
 class TestForActions(unittest.TestCase):
 
 
-##TODO finish testing with setting before valid for loops are tested
     def test_parsing_and_actions(self):
-        setup = [
-            'set x to 1',
-            'set y to 54',
-        ]
-
+        
         lines_to_test = [
-            ExpectedResult('for 2 times set x to add 10 to x',for_action.ForAction,20,'For loop twice')
+            ExpectedResult('for 2 times add 10 to 10',for_action.ForAction, math_action.AddAction ,'For loop addition'),
+            ExpectedResult('for 5 times subtract 20 from 20',for_action.ForAction, math_action.SubAction,'For loop subtraction'),
+            ExpectedResult('for 5 times set x to 34',for_action.ForAction, set_action.SetAction,'For loop subtraction')
         ]
-
-        for line in setup:
-            parsed_action = set_action.SetAction.parse_from_line(line.split())
-            my_variables = variables.Variables()
-            action_res = parsed_action.action(my_variables)
 
         for expected_res in lines_to_test:
             my_variables = variables.Variables()
-            with self.subTest(msg=f'Test {expected_res.reason}: "{expected_res.line}" is of type {expected_res.type} with result {expected_res.expected_result}'):
+            with self.subTest(msg=f'Test {expected_res.reason}: "{expected_res.line}" is of type {expected_res.type}'):
                 parsed_action = for_action.ForAction.parse_from_line(expected_res.items)
-                self.assertIsInstance(parsed_action, expected_res.type )
-                action_res = parsed_action.action(my_variables)
-                self.assertEqual(action_res, expected_res.expected_result)
+                self.assertIsInstance(parsed_action, expected_res.type)
+                self.assertIsInstance(parsed_action.next_action,expected_res.next_action)
 
     def test_exceptions(self):
         lines_expected_exception = [
